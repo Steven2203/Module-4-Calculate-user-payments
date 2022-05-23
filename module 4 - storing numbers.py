0fa21886e86402b6094e8f509ebec794a021aa6f
Steven van Veldhoven <42697075+Steven2203@users.noreply.github.com>
2022-05-23 18:57:32 +0200

    Add files via upload

---
+++ module 4 - storing numbers.py
@@ -0,0 +1,33 @@
+#-------------------------------------------------------------------------------
+# Name:        module 4 - Calculate monthly payments
+# Purpose:     Calculate monthly payments using a calculation
+# Author:      Steven
+# Created:     22-05-2022
+# Copyright:   (c) Steven 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+##Still working on following module. So far i have the code underneath.
+##Figuring out the calculation needed.
+
+##Type casting variables.
+
+int(Salary)
+int(monthlyPayment)
+int(numberPayments)
+float(interestRate)
+
+##Asking for input from the user.
+
+Salary = input("Enter the loancost")
+monthlyPayment = input("Enter the amount of monthly payment")
+numberPayments = input("Enter the number of payments")
+interestRate = 0.05
+
+##Calculating the final value.
+
+finalCalculation = (interestRate) + (NumberPayments) + 1 / (interestRate) + 1 + (NumberPayments) -1
+monthlyPayment == finalCalculation
+print (finalCalculation)
+
+
